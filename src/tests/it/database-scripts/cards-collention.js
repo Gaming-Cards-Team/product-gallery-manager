695916a56442da6896436684f69fd69f418203a1
cristtynu <35372981+cristtynu@users.noreply.github.com>
2020-04-16 17:05:53 -0500

    Tests Pipeline

--- src/tests/it/database-scripts/cards-collention.js
+++ src/tests/it/database-scripts/cards-collention.js
@@ -2,7 +2,7 @@ import { Card } from "../../../main/dataaccess/entities/card"
 
 export const insertCards = (done) => {
 
-    const arr = [{_id: 1,name: "Card 1"},{_id: 2,name: "Card 2"},{_id: 3,name: "Card 3"},{_id: 4,name: "Card 4"}];
+    const arr = [{_id: 1,name: "Card 1"},{_id: 2,name: "Card 2"},{_id: 3,name: "Card 3"},{_id: 4,name: "Card 4"},{_id: 5,name: "Card 5"}];
     Card.insertMany(arr, done);
 }
 
