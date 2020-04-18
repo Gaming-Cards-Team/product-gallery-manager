1767f8ccb64bf3f65e551b5db7b0535df0a2be62
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-18 17:30:51 -0500

    Cors config

--- src/main/index.js
+++ src/main/index.js
@@ -9,8 +9,8 @@ const swaggerDocument = yamljs.load('swagger.yml')
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api/v1', router);
 app.use(cors)
+app.use('/api/v1', router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
