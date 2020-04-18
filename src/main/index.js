d4b63ba4addba3db2dfae64b960e9e0c55e81b2a
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-17 21:23:30 -0500

    Testing Swagger

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import router from './endpoints/routers.js'
+import swaggerUi from 'swagger-ui-express'
+import yamljs from 'yamljs'
+
+const swaggerDocument = yamljs.load('swagger.yml')
 
 const app = express();
 
-app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
