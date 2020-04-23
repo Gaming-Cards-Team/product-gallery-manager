d4c6f1da89419ea5aed2cbf0e36c890e60c87911
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-23 12:07:59 -0500

    Card-74 [@twjmgorduez, @daviddavilatw, @cristtynu] Reads STAR_WARS_REST_API_URL from .env

--- src/main/dataaccess/product-gallery-rest-consumer.js
+++ src/main/dataaccess/product-gallery-rest-consumer.js
@@ -1,11 +1,13 @@
 import request from "request";
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
 module.exports = function () {
 
     return {
       
         consumeStarWarsGalleryAPI(done) {
-          request.get("https://swdestinydb.com/api/public/cards/",(error, response, body) => {
+          request.get(`${process.env.STAR_WARS_REST_API_URL}cards/`,(error, response, body) => {
               const cardsResult = JSON.parse(body);
               done(error, cardsResult);
             });
