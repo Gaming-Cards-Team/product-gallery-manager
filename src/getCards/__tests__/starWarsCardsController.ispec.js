13b1e1a4e1143d083efbedd9f3a4ef04b12d32c2
David Davila <david.davila@thoughtworks.com>
2020-04-24 11:46:34 -0500

    Card-74 [@twjmgorduez, @daviddavilatw, @cristtynu] Sets up the test config and fix a typo

---
+++ src/getCards/__tests__/starWarsCardsController.ispec.js
@@ -0,0 +1,18 @@
+const mockStarWarCards = [{ id: "001", name: "darth vader", image: "url-image" }];
+import MockExpressResponse from 'mock-express-response'
+import MockExpressRequest  from 'mock-express-request'
+import starWarCardsService from "../starWarCardsService";
+starWarCardsService.getStarWarsCards = jest.fn(() => mockStarWarCards);
+import getCards from "../starWarsCardsController";
+
+describe("star Wars cards controller", () => {
+  it("should get all cards", async () => {
+    const response = new MockExpressResponse()
+    const request = new MockExpressRequest()
+
+    await getCards(request, response);
+
+    expect(starWarCardsService.getStarWarsCards).toHaveBeenCalledTimes(1);
+  });
+});
+
