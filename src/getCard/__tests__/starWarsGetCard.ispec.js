687b6c22d793f90e1ecceebbd9c3113ddaa5a988
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-05-06 13:17:06 -0500

    Card-75 [@twjmgorduez] Comsumes a card from Star Wars API

--- src/getCard/__tests__/starWarsGetCard.ispec.js
+++ src/getCard/__tests__/starWarsGetCard.ispec.js
@@ -16,9 +16,18 @@ describe("Star Wars Card", () => {
     const request = supertest(app);
     const expectedStarWarsCard =
     {
-      "id": "01002",
-      "name": "Trooper",
-      "image": "https://swdestinydb.com/bundles/cards/en/01/01002.jpg"
+      id: "01002",
+      name: "First Order Stormtrooper",
+      image: "https://swdestinydb.com/bundles/cards/en/01/01002.jpg",
+      affiliation: "Villain",
+      card: 2,
+      color: "Red",
+      points: "7",
+      rarity: "Rare",
+      type: "Character",
+      uniqueness: "Non/Unique",
+      text: "",
+      subtitle: null,
     };
 
     const response = await request.get('/api/v1/card?cardId=01002').set('Accept', 'application/json');
--- src/getCard/starWarCardService.js
+++ src/getCard/starWarCardService.js
@@ -1,9 +1,29 @@
 import  starWarsCardClient from "./starWarsCardClient";
+import htmlToText from 'html-to-text'
 
 const getStarWarsCard = async (cardId) => {
   const card = await starWarsCardClient.getStarWarsCard(cardId);
-  const cardResult = { id: card.code, name: card.name, image: card.imagesrc };
-  return cardResult;
+  return toOwnCard(card);
+};
+
+const toOwnCard = (card) => {
+    const uniqueness = card.is_unique ? 'Unique' : 'Non/Unique'
+    const color =  `${card.faction_code.charAt(0).toUpperCase()}${card.faction_code.slice(1)}`;
+    const text = htmlToText.fromString(card.text, {wordwrap: 130});
+    return {
+      id: card.code, 
+      name: card.name, 
+      image: card.imagesrc,
+      affiliation: card.affiliation_name,
+      card: card.position,
+      color: color,
+      points: card.points,
+      rarity: card.rarity_name,
+      type: card.type_name,
+      uniqueness: uniqueness,
+      text: text,
+      subtitle: card.subtitle,
+    }
 };
 
 module.exports.getStarWarsCard = getStarWarsCard
